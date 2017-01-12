d11daf0b234296d37ce113e6914bac8b42ac4c6b
Arnold Daniels <arnold@jasny.net>
2017-01-12 23:52:12 +0100

    Moved view logic from controller library to this lib

---
+++ src/View/Twig.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Jasny\View;
+
+use Jasny\ViewInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Abstraction to use Twig with PSR-7
+ */
+class Twig implements ViewInterface
+{
+    /**
+     * Twig environment
+     * @var \Twig_Environment
+     */
+    protected $twig;
+
+
+    /**
+     * Class constructor
+     * 
+     * @param \Twig_Environment|array $options
+     */
+    public function __construct($options)
+    {
+        if (is_array($options)) {
+            if (!isset($options['path'])) {
+                throw new \BadMethodCallException("'path' option is required");
+            }
+            
+            $loader = new \Twig_Loader_Filesystem($options['path']);
+            $twig = new \Twig_Environment($loader);
+        } else {
+            $twig = $options;
+        }
+        
+        if (!$twig instanceof \Twig_Environment) {
+            throw new \InvalidArgumentException("Was expecting an array with options or a Twig_Environment, got a "
+                . (is_object($twig) ? get_class($twig) . ' ' : '') . gettype($twig));
+        }
+        
+        $this->twig = $twig;
+    }
+    
+    /**
+     * Get Twig environment
+     *
+     * @return \Twig_Environment
+     */
+    public function getTwig()
+    {
+        return $this->twig;
+    }
+
+    
+    /**
+     * Assert valid variable, function and filter name
+     * 
+     * @param string $name
+     * @throws \InvalidArgumentException
+     */
+    protected function assertViewVariableName($name)
+    {
+        if (!is_string($name)) {
+            $type = (is_object($name) ? get_class($name) . ' ' : '') . gettype($name);
+            throw new \InvalidArgumentException("Expected name to be a string, not a $type");
+        }
+
+        if (!preg_match('/^[a-z]\w*$/i', $name)) {
+            throw new \InvalidArgumentException("Invalid name '$name'");
+        }
+    }
+
+    /**
+     * Expose a function to the view.
+     *
+     * @param string        $name      Function name in template
+     * @param callable|null $function
+     * @param string        $as        'function' or 'filter'
+     * @return $this
+     */
+    public function expose($name, $function = null, $as = 'function')
+    {
+        $this->assertViewVariableName($name);
+        
+        if ($as === 'function') {
+            $function = new \Twig_SimpleFunction($name, $function ?: $name);
+            $this->getTwig()->addFunction($function);
+        } elseif ($as === 'filter') {
+            $filter = new \Twig_SimpleFilter($name, $function ?: $name);
+            $this->getTwig()->addFilter($filter);
+        } else {
+            $not = is_string($as) ? "'$as'" : 'a ' . gettype($as);
+            throw new \InvalidArgumentException("You should create either a 'function' or 'filter', not $not");
+        }
+
+        return $this;
+    }
+
+    /**
+     * Add the official Twig extension and Jansy Twig extensions.
+     * 
+     * @link https://github.com/twigphp/Twig-extensions
+     * @link https://github.com/jasny/twig-extensions
+     * 
+     * @return $this
+     */
+    public function addDefaultExtensions()
+    {
+        $extensions = [
+            'Twig_Extensions_Extension_Array',
+            'Twig_Extensions_Extension_Date',
+            'Twig_Extensions_Extension_I18n',
+            'Twig_Extensions_Extension_Intl',
+            'Twig_Extensions_Extension_Text',
+            'Jasny\Twig\DateExtension',
+            'Jasny\Twig\PcreExtension',
+            'Jasny\Twig\TextExtension',
+            'Jasny\Twig\ArrayExtension'
+        ];
+        
+        foreach ($extensions as $class) {
+            if (class_exists($class)) {
+                $this->getTwig()->addExtension(new $class());
+            }
+        }
+    }
+
+    /**
+     * Add a twig extension
+     * 
+     * @param \Twig_ExtensionInterface $extension
+     * @return $this
+     */
+    public function addExtension(\Twig_ExtensionInterface $extension)
+    {
+        $this->getTwig()->addExtension($extension);
+    }
+
+
+    /**
+     * Render and output template
+     *
+     * @param ResponseInterface $response
+     * @param string            $name      Template name
+     * @param array             $context   Template context as associated array
+     * @return ResponseInterface
+     */
+    public function view(ResponseInterface $response, $name, array $context = [])
+    {
+        if (!pathinfo($name, PATHINFO_EXTENSION)) {
+            $name .= '.html.twig';
+        }
+
+        $twig = $this->getTwig();
+        $tmpl = $twig->loadTemplate($name);
+
+        $contents = $tmpl->render($context);
+        
+        $newResponse = $response->withHeader('Content-Type', 'text/html; charset=' . $twig->getCharset());
+        $newResponse->getBody()->write($contents);
+        
+        return $newResponse;
+    }
+}
---
+++ src/ViewInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Jasny;
+
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Interface for abstracting over template engine.
+ */
+interface ViewInterface
+{
+    /**
+     * Expose a function to the view.
+     *
+     * @param string        $name      Function name in template
+     * @param callable|null $function
+     * @return $this
+     * @throws \BadMethodCallException if function can't be exposed
+     */
+    public function expose($name, $function = null);
+    
+    /**
+     * Render and output template
+     *
+     * @param ResponseInterface $response
+     * @param string            $name      Template name
+     * @param array             $context   Template context as associated array
+     * @return ResponseInterface
+     */
+    public function view(ResponseInterface $response, $name, array $context = []);
+}
